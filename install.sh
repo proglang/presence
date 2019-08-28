b7d06d7fa8dad67f4383ff17ec735b734104712c
Stefan Schweizer <schweizs@uni-freiburg.de>
2019-08-28 11:55:43 +0200

    script update

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
-composer install --no-dev --optimize-autoloader
+#!/bin/sh
+
+usage() { echo "Usage: $0 [-i]" 1>&2; exit 1; }
+
+install=0
+while getopts ":i:" opt; do
+  case $OPTARG in
+    i) install=1
+    ;;
+    *) usage
+    ;;
+  esac
+done
+
+printf "Argument install is %s\n" "$install"
+if [ $install == 1 ]
+then
+    composer install --no-dev --optimize-autoloader
+fi
 php artisan api:config
 php artisan api:install
