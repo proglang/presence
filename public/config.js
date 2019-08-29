c5ce13233c9e9bee961b4510e7110c800c218334
Stefan Schweizer <schweizs@uni-freiburg.de>
2019-08-29 04:19:10 +0200

    updates

--- public/config.js
+++
@@ -1,4 +0,0 @@
-const app_config= {
-    api_path:null,
-    app_path:null,
-};
---
+++ public/config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm config.js
+function set_config(){
+    echo 'const app_config= {' >> "config.js";
+    if [ ! -z "$api_url" ]
+    then
+        echo "api_path:'${api_url}'," >> "config.js";
+    fi
+    if [ ! -z "$app_url" ]
+    then
+        echo "app_path:'${app_url}'," >> "config.js";
+    fi
+    echo '};' >> "config.js";
+
+}
+
+echo "Enter absolute API URL: e.g. '/api'"
+read api_url
+echo "Enter absolute APP URL: e.g. '/'"
+read app_url
+
+set_config
