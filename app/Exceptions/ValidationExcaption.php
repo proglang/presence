1a2d8b59358e7a360fc9ddb38e6ca0558d1a8a8b
Stefan Schweizer <schweizs@uni-freiburg.de>
2019-08-31 10:20:23 +0200

    hide _hash in returns

--- app/Exceptions/ValidationExcaption.php
+++ app/Exceptions/ValidationExcaption.php
@@ -8,18 +8,35 @@
 namespace App\Exceptions;
 
 use App\Http\Response\Response;
-class ValidationException extends RenderExceptionBase implements IRenderException{
+
+class ValidationException extends RenderExceptionBase implements IRenderException
+{
 
     public function __construct($human, $err, $args)
     {
+        $keys = [];
+        foreach ($err as $key) {
+            $newkey = str_replace("_hash", "", $key);
+            if ($newkey != $key) {
+                foreach ($args[$key] as $k => $v) {
+                    if (gettype($v) == "string") {
+                        $args[$key][$k] = str_replace("_hash", "", $v);
+                    }
+                }
+                $args[$newkey] = array_merge($args[$newkey] ?? [], $args[$key]);
+                unset($args[$key]);
+            }
+            $keys[] = $newkey;
+        }
         $data = [
             'text' => $human,
-            'keys'=>$err,
-            'args'=>$args
+            'keys' => $keys,
+            'args' => $args
         ];
-        parent::__construct('error','Validation Error', 422, '', $data);
+        parent::__construct('error', 'Validation Error', 422, '', $data);
     }
-    public function render():Response {
+    public function render(): Response
+    {
         return $this->renderError("validation", "ValidationException");
     }
 }
