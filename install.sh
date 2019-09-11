0e94b878c088942bcfed529968f4583351ded9ef
Stefan Schweizer <schweizs@uni-freiburg.de>
2019-09-12 00:04:36 +0200

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo docker-compose -f "docker-compose.yml" down
-sudo docker-compose -f "docker-compose.yml" up -d --build
-sudo docker-compose -f "docker-compose.yml" down
-sudo docker-compose -f "docker-compose.yml" up -d
+docker-compose -f "docker-compose.yml" down
+docker-compose -f "docker-compose.yml" up -d --build
+docker-compose -f "docker-compose.yml" down
+docker-compose -f "docker-compose.yml" up -d
