9247c7f25255785c3cd1d61854c4edc2b19bd8f4
Stefan Schweizer <schweizs@uni-freiburg.de>
2019-09-12 17:02:34 +0200

    .

--- install.sh
+++ install.sh
@@ -3,7 +3,4 @@ echo Installing App
 
 docker-compose -f "docker-compose.yml" down
 docker-compose -f "docker-compose.yml" up -d --build
-docker-compose -f "docker-compose.yml" down
-docker-compose -f "docker-compose.yml" up -d
-
 chmod -R o+w ./image/api
